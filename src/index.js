d8fb183dd59c6e1f51645a1796ccd8e1aae1fdc9
fgriff <>
2021-03-03 21:18:55 +0300

    feat: implement towel-sort function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+
+  if (matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 === 0) {
+        res = res.concat(matrix[i]);
+      } else {
+        res = res.concat(matrix[i].reverse());
+      }
+    }
+  } else {
+      return res;
+  }
+  
+  return res;
 }
